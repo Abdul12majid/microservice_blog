db72542554c17464e57364ecdff850f438d3d45d
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-19 13:37:25 +0100

    update views

--- users_service/user_app/views.py
+++ users_service/user_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def root(request, format=None):
+    return Response({
+        'All': "no urls yet",
+        
+    })
