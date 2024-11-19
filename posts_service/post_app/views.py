63dce91f59d83d22e450a9e7c1fc5bb3fd275e53
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-19 19:39:44 +0100

    auth JWT in posts_service

--- posts_service/post_app/views.py
+++ posts_service/post_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.urls import reverse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 @api_view(['GET'])
 def root(request, format=None):
     return Response({
-        'All': "Thou",
-        
-        
+        'All': "Thou",       
     })
+
+
--- posts_service/posts_service/settings.py
+++ posts_service/posts_service/settings.py
@@ -72,6 +72,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'posts_service.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
