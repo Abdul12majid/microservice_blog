b2cbbc9b5e58986e7a8cba19829fdc6ed90a7ce8
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-23 12:21:37 +0100

    created commentmodel

--- comments_service/comment_app/models.py
+++ comments_service/comment_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post_id = models.UUIDField() 
+    user_id = models.IntegerField() 
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by User {self.user_id} on Post {self.post_id}"
