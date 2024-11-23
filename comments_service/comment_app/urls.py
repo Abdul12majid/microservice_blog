4da65c833a8e2bacbfc776c0fd28a89daaf12a56
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-23 12:44:08 +0100

    post comment

--- comments_service/comment_app/urls.py
+++ comments_service/comment_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('comments/', views.comments_list_create, name='comments_list_create'),
+    path('comments/<uuid:comment_id>/', views.comment_detail, name='comment_detail'),
     
 ]
--- comments_service/comment_app/views.py
+++ comments_service/comment_app/views.py
@@ -18,3 +18,26 @@ def comments_list_create(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def comment_detail(request, comment_id):
+    try:
+        comment = Comment.objects.get(id=comment_id)
+    except Comment.DoesNotExist:
+        return Response({"error": "Comment not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response({"message": "Comment deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
